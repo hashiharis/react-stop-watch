b211adea2932dd067b069c1edad3e5534153b5fd
hashiharis <100787257+hashiharis@users.noreply.github.com>
2024-09-07 09:04:23 +0530

    timer code

--- react-stop-watch/src/components/Timer.jsx
+++ react-stop-watch/src/components/Timer.jsx
@@ -1,44 +1,75 @@
+import { useEffect, useRef, useState } from "react";
+import { TfiControlPause, TfiControlPlay } from "react-icons/tfi";
+import './timer.css'
 
-import { useRef, useState } from "react"
+export const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [isTimerActive,setIsTimerStopped]=useState(false);
+  const timerRef=useRef(null)
 
-export const Timer=()=>{
-    const [timerStart,setTimerStart]=useState("00:05:00")
-
-
-
-    const handleChange=(e)=>{
-    const input= e.target.value
-    const arr=input.split(":")
-    const lastElement=arr.slice(-1)
-    console.log(lastElement)
-    // console.log(input)
-
- setTimerStart()
-      
-       
-};
-
-
-// const handleKeyDown = (e) => {
-//     // Prevent backspace key
-//     if (e.key === 'Backspace') {
-//       e.preventDefault();
-//     }
-//   };
+  const handleChange = (e) => {
+    
+    setSeconds(e.target.value)
    
-    return(
-        <div className="timer">
-            <input placeholder="00:00:00" dir="rtl" inputMode="numeric" maxLength="8" value={timerStart}
-            onChange={handleChange}
-            />
-            <div>
-                <button>Pause</button>
-                <button>Reset</button>
-            </div>
+  };
+
+  useEffect(()=>{
+    if(isTimerActive&&seconds>0){
+        timerRef.current=setInterval(()=>{
+           setSeconds(seconds=>seconds-1)
+        },1000)
+    }
+    else if(seconds===0){
+        clearInterval(timerRef.current)
+    }
+
+    return()=>{
+        clearInterval(timerRef.current)
+    }
+  },[isTimerActive,seconds])
+
+  const handlePlay=()=>{
+       
+    setIsTimerStopped(isTimerActive=>!isTimerActive)
+    
+    
+  }
+
+  const handleReset=()=>{
+    setIsTimerStopped(true)
+    setSeconds(0)
+    setIsActive(isActive=>!isActive)
+  }
+  return (
+    <div className="timer">
+        <button className="input-btn" onClick={()=>{handlePlay()
+             setIsActive(true)
+        }}>
+        <input
+        type="number"
+        name="seconds"
+        value={seconds}
+        onChange={handleChange}
+      />
+        </button>
+     
+      {isActive ? (
+        <div className="timer-btns">
+          <button onClick={()=>{handlePlay()
+            setIsActive(true)
+          }}>{isTimerActive?<TfiControlPause />:<TfiControlPlay />}</button>
+          <button onClick={handleReset}>Reset</button>
         </div>
-
-    )
-}
-
-
-  
+      ) : (
+        <div className="click-container">
+          <button onClick={()=>{
+            handlePlay()
+            setIsActive(isActive=>!isActive)
+          }}><TfiControlPlay size="1.5rem"/></button>
+            
+        </div>
+      )}
+    </div>
+  );
+};
--- react-stop-watch/src/components/WatchContainer.jsx
+++ react-stop-watch/src/components/WatchContainer.jsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import { Stopwatch } from "./Stopwatch";
 import "./watchcontainer.css";
 import { Timer } from "./Timer";
+import { IoStopwatchOutline } from "react-icons/io5";
+import { IoIosTimer } from "react-icons/io";
 export const WatchContainer = () => {
   const [isTimer, setIsTimer] = useState(false);
 
   return (
     <div className="stopwatch-container">
       <div className="option-buttons">
-        <button onClick={()=>setIsTimer(true)}>Timer</button>
-        <button onClick={()=>setIsTimer(false)}>Stopwatch</button>
+        <button onClick={()=>setIsTimer(true)}><IoIosTimer/>Timer</button>
+        <button onClick={()=>setIsTimer(false)}><IoStopwatchOutline />Stopwatch</button>
       </div>
       {isTimer ? (
         <div className="counter-container">
